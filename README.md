e6e4a354339be1f78dbf1423d0b028e4cbdbc60b
stornf <38433142+stornf@users.noreply.github.com>
2019-03-11 18:44:29 +0300

    stornf

---
+++ README.md
@@ -0,0 +1,10 @@
+# AZinstaller
+
+توقيع التطبيقات من خلال رابط ال ipa وتثبيتها من داخل الايفون دون الحاجة للابتوب او سيديا امبكتر
+
+المطورين
+
+# Azozz ALFiras & Abdullah Aahmed
+
+ <a href="https://twitter.com/iFenixDev"> Ahmed Salah </a>
+ 
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <title>CydiaImpactor</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Cairo" rel="stylesheet">
+</head>
+<body>
+
+
+
+    <P style="
+      color: #000000;
+      background:#f3f3f3;
+      font-size:25px;
+      text-decoration: none;" href="#"> CydiaImpactor </P>
+
+
+<hr>
+<div class="info">
+  <form  action="installer.php" method="POST">
+    <input type="url" name="url"  required="required"  placeholder="Direct URL to .ipa ">
+      <button class="download" type="submit" name="submit" >install</button>
+  </form>
+</div>
+
+<div>
+   <p> Follow me us on Twitter for news and update ! </p>
+
+  <button id="developer" href="https://twitter.com/its_Fywre"> Ahmed Salah </button>
+
+</div>
+<p id="V" >V0.5</p>
+
+</body>
+</html>
