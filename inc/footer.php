036cfa97c5761c135909d69f218fee09a4deb963
stornf <38433142+stornf@users.noreply.github.com>
2019-03-11 18:57:53 +0300

    n

---
+++ inc/footer.php
@@ -0,0 +1,9 @@
+<hr />
+  <div class="container text-center Txtfooter">
+    <h5 id="wells" class="page-header">تصميم وبرمجة</h5>
+    الحقوق محفوظة نواف المطيري<br/>
+    <!--<a class="btn btn-xs btn-default" style="padding: 5px;" href="https://twitter.com/private_apple">للمتابعة اضغط هنا</a> -->
+
+  </div>
+  </body>
+  </html>
